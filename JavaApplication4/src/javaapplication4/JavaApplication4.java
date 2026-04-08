6b9a97fb7d9126f4d4e03fc70c4fd0965143fb6a
malmadhoo <mohammedalmadhoon000@gmail.com>
2026-04-08 16:45:36 +0300

    added base folder

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,13 @@
+
+package javaapplication4;
+
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
