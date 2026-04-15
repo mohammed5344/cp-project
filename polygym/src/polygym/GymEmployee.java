0b0abc6cf09a2f80599af8280ae18f6aa2e5bc36
malmadhoo <mohammedalmadhoon000@gmail.com>
2026-04-15 17:10:51 +0300

    added employee and trainer classes

--- polygym/src/polygym/GymEmployee.java
+++ polygym/src/polygym/GymEmployee.java
@@ -2,5 +2,41 @@
 package polygym;
 
 public class GymEmployee {
+    private int id;
+    private int employeeCounter;
+    private PersonalDetails personalDetails;
+    private double salary;
+    public GymEmployee(PersonalDetails personalDetails) {
+        
+        if(personalDetails == null) {
+            throw new IllegalArgumentException("invalid personal details");
+        }
+        
+        employeeCounter++;
+        this.id = employeeCounter;
+        this.personalDetails = personalDetails;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public PersonalDetails getPersonalDetails() {
+        return personalDetails;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
     
+    public void setSalary(double salary) {
+        if(salary <= 0) {
+            throw new IllegalArgumentException("salary cannot be negative");
+        }
+        if(salary > 999999999) {
+            throw new IllegalArgumentException("salary is too larg");
+        }
+        
+        this.salary = salary;
+    }
 }
--- polygym/src/polygym/PersonalTrainer.java
+++ polygym/src/polygym/PersonalTrainer.java
@@ -1,6 +1,30 @@
 
 package polygym;
 
+import java.util.ArrayList;
+
 public class PersonalTrainer {
+    private ArrayList<GymMember> assignedMembers = new ArrayList<>();
     
+    private void assignMember(GymMember member) {
+        if(member == null) {
+            throw new IllegalArgumentException("invalid member");
+        }
+        this.assignedMembers.add(member);
+    }
+    
+    private void removeMember(GymMember member) {
+        if(member == null) {
+            throw new IllegalArgumentException("invalid member");
+        }
+        if(!this.assignedMembers.contains(member)) {
+            throw new IllegalArgumentException("member not found within the assigned members to this trainer");
+        }
+        
+        this.assignedMembers.remove(member);
+    }
+    
+    private ArrayList<GymMember> getAssignedMembers() {
+        return this.assignedMembers;
+    }
 }
