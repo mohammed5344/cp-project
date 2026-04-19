30f0abd94b73778f54cc25a336ab66daf3e3b3c5
mohammed5344 <39929536+mohammed5344@users.noreply.github.com>
2026-04-19 22:33:09 +0300

    fixed

--- polygym/src/polygym/StudentMember.java
+++ polygym/src/polygym/StudentMember.java
@@ -1,34 +1,4 @@
-import java.util.Date;
 public class StudentMember extends GymMember {
 
-    private int studentId;
-    private String major;
-    private String sportTeam;
-
-    public StudentMember(PersonalDetails personalDetails, int studentId) {
-        super(personalDetails);
-        this.studentId = studentId;
-        this.major = null;
-        this.sportTeam = "None";
-    }
-
-    public int getStudentId(){
-        return studentId;
-    }
-
-    public String getMajor(){
-        return major;
-    }
-
-    public String getSportTeam(){
-        return sportTeam;
-    }
-
-    public void setMajor(String major) {
-        this.major = major;
-    }
-
-    public void setSportTeam(String sportTeam) {
-        this.sportTeam = sportTeam;
-    }
+    
 }
