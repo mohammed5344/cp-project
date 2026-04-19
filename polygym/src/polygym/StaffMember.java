5072182edeca61c38e21c2affcfef6e6af19a8a9
mohammed5344 <39929536+mohammed5344@users.noreply.github.com>
2026-04-19 22:32:52 +0300

    fixed

--- polygym/src/polygym/StaffMember.java
+++ polygym/src/polygym/StaffMember.java
@@ -2,34 +2,5 @@ import java.util.Date;
 
 public class StaffMember extends GymMember {
 
-    private int staffId;
-    private String position;
-    private String department;
-
-    public StaffMember(PersonalDetails personalDetails, int staffId){
-        super(personalDetails);
-        this.staffId = staffId;
-        this.position= null;
-        this.position= null;
-    }
-
-    public int getStaffId(){
-        return staffId;
-    }
-
-    public String getPosition(){
-        return position;
-    }
-
-    public String getDepartment(){
-        return department;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public void setDepartment(int buildingNum, int floor, int room){
-        department = "Building " + buildingNum + ", Floor " + floor + ", Room " + room;
-    }
+    
 }
